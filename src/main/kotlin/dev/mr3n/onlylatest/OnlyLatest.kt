fde0cc4f4e254b51a8552cd201b64b4607a8f89d
moruch4nn <moruch4nn@gmail.com>
2023-03-29 04:44:47 +0900

    🏎コードの軽量化。

--- src/main/kotlin/dev/mr3n/onlylatest/OnlyLatest.kt
+++ src/main/kotlin/dev/mr3n/onlylatest/OnlyLatest.kt
@@ -1,18 +1,16 @@
 package dev.mr3n.onlylatest
 
-import com.google.inject.Inject
 import com.velocitypowered.api.event.Subscribe
 import com.velocitypowered.api.event.proxy.ProxyPingEvent
 import com.velocitypowered.api.network.ProtocolVersion
 import com.velocitypowered.api.plugin.Plugin
-import com.velocitypowered.api.proxy.ProxyServer
 import com.velocitypowered.api.proxy.server.ServerPing
 
 @Plugin(id = "onlylatest", name = "OnlyLatest")
-class OnlyLatest @Inject constructor(private val server: ProxyServer) {
+class OnlyLatest {
+    private val protocolVersion = ProtocolVersion.values().maxByOrNull { it.protocol }!!
     @Subscribe
     fun on(event: ProxyPingEvent) {
-        val protocolVersion = ProtocolVersion.values().maxByOrNull { it.protocol }!!
         event.ping = event.ping.asBuilder().version(ServerPing.Version(protocolVersion.protocol,protocolVersion.name)).build()
     }
 }
